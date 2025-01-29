d5cfe20c99903c110a3d531d4fdccf58ba1d8f52
Snehal2104 <126266160+Snehal2104@users.noreply.github.com>
2025-01-29 07:03:17 +0530

    Add files via upload

---
+++ src/app/frist-component/frist-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-frist-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './frist-component.component.html',
+  styleUrl: './frist-component.component.css'
+})
+export class FristComponentComponent {
+
+}
